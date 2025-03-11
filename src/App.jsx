b7911be4f1513409a7440b9f3870a61c7aa8601e
VHelloKittyV <strangetip@gmail.com>
2025-03-11 11:15:38 +0200

    add Pass Confirm

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
 import { useForm } from "react-hook-form";
-import "./App.css";
 
 export default function App() {
   const {
     register,
     handleSubmit,
-    setError,
     formState: { errors, isSubmitting },
+    reset,
+    getValues
   } = useForm();
 
   const onSubmit = async (data) => {
@@ -22,11 +21,15 @@ export default function App() {
   return (
     <>
       <div className="max-w-xl mx-auto grid p-6">
+
         <hgroup className="text-center mb-6">
           <h1 className="text-3xl ">Form</h1>
           <p>with React Hook Form</p>
         </hgroup>
-        <form className="place-self-center flex flex-col w-full gap-3 bg-gray-100 p-12 rounded-lg" action="" onSubmit={handleSubmit(onSubmit)}>
+
+        <form 
+          className="place-self-center flex flex-col w-full gap-3 bg-gray-100 p-12 rounded-lg" 
+          onSubmit={handleSubmit(onSubmit)}>
           {/* Email */}
           <label className="text-blue-900">Email:</label>
           <input
@@ -36,9 +39,9 @@ export default function App() {
                 if(!value.includes("@")) return "Invalid Email"
               }
             })}
-            type="text"
-            className="px-3 py-2 border"
+            type="email"
             placeholder="Email"
+            className="px-3 py-2 border"
           />
           {errors.email && <div className="text-red-600">{errors.email.message}</div>}
 
@@ -50,14 +53,29 @@ export default function App() {
               minLength:{value:6, message: "Password is too small"}
             })}
             type="password"
-            className="px-3 py-2 border"
             placeholder="Password"
+            className="px-3 py-2 border"
           />
           {errors.password && <div className="text-red-600">{errors.password.message}</div>}
+          
+          {/* Confirm Password */}
+          <label className="text-blue-900">Confirm Password:</label>
+          <input 
+            {...register("confirmPassword", {
+              required: "is required",
+              validate:(value)=>getValues("password")===value || "Password do not match"
+            })}
+            type="password"
+            placeholder="Confirm password"
+            className="px-3 py-2 border"
+          />
+          {errors.confirmPassword && <div className="text-red-600">{errors.confirmPassword.message}</div>}
 
           {/* Select */}
           <label  className="text-blue-900 ">Select:</label>
-          <select className="px-3 py-2 border" {...register("select",
+          <select 
+            className="px-3 py-2 border" 
+            {...register("select",
               {validate: (value) => value !== "" || "Select is required"})
             }>
             <option value=''></option>
@@ -67,7 +85,8 @@ export default function App() {
           </select>
           {errors.select && <div className="text-red-600">{errors.select.message}</div>}
 
-          <button className="btn mt-5 text-white px-3 py-2 cursor-pointer bg-blue-950 hover:opacity-90" disabled={isSubmitting} type="submit">
+          <button 
+            className="btn mt-5 text-white px-3 py-2 cursor-pointer bg-blue-950 hover:opacity-90" disabled={isSubmitting} type="submit">
             {isSubmitting? "Loading...": "Submit"}
           </button>
         </form>
